91767b8d8aff9129015f1b1c4c6096183f421e7c
Omar <74269367+3mDV@users.noreply.github.com>
2025-11-18 11:56:48 +0300

    update file
    add panda for data form

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+import panda as pd
+def arabic_fonts(text: str) -> str:
+  """Fix almost every font problems 
+    :return: string"""
+  pass
+
+def reader(path: str) -> ...:
+  """read contents from pdf file"""
+  pass
+
+def contract_no(text: str) -> str:
+  pass
+
+def tenancy_dates(text: str) -> str:
+  pass
+
+def tenancy_name(text: str) -> str:
+  pass
+
+def founder_name(text: str) -> str:
+  pass
+
+def national_address(text: str) -> str:
+  pass
+
+def payments_schedule(text: str) -> str:
+  pass
+
+def data_form(keys:list, values:list) -> ...:
+  pass
+
+def file_finder(path: str) -> ...:
+  pass
+
 
